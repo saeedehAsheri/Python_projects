0cdd8c7876b8398dd3a2753a19ed49d7fa3e5a1b
Saeedeh Asheri <saeedeh@Saeedehs-MacBook-Pro.local>
2026-04-01 16:57:35 +0100

    io utils updated

--- CallMeMaybe/src/io_utils.py
+++ CallMeMaybe/src/io_utils.py
@@ -1,9 +1,12 @@
 from typing import Any
 import json
 from pathlib import Path
+from pydantic import ValidationError
+from models import FunctionCall, FunctionCallResult, FunctionDefinition
 
 def load_json_file(path: Path)-> Any:
     """
+    reading the JSON file
     """
     try:
         with open(path, "r") as file:
@@ -14,3 +17,17 @@ def load_json_file(path: Path)-> Any:
         print("Error: The file 'data.json' was not found.")
     except json.JSONDecodeError:
         print("Error: Failed to decode JSON from the file.")
+
+def write_json_file(path: Path, results: list[FunctionCallResult])-> None:
+    """Write valid results in a JSON file."""
+
+    path.parent.mkdir(parents= True, exist_ok= True)
+
+    data = [res.model_dump() for res in results]
+    try:
+        with open(Path, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent= 2, ensure_ascii= False)
+    except OSError:
+        print(f"Couldn't write output file to '{path}'")
+
+
